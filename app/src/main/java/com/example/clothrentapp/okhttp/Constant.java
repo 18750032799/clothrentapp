a3ec225e9ecfde4f018d50f191dd8c9b4a81f9f0
lzh <598665303@qq.com>
2019-10-22 23:44:20 +0800

    change ip

--- app/src/main/java/com/example/clothrentapp/okhttp/Constant.java
+++ app/src/main/java/com/example/clothrentapp/okhttp/Constant.java
@@ -4,7 +4,8 @@ public class Constant {
 
 //    public static String BASE_URL = "http://172.20.10.3:8088/Demo2_war_exploded/";
 
-    public static String BASE_URL = "http://172.20.10.2:8080/clothService";
+    public static String BASE_URL = "http://192.168.0.102:8080/clothService";
+//    public static String BASE_URL = "http://172.20.10.2:8080/clothService";
 
     public static String user_login = BASE_URL + "/LoginServlet";
 
